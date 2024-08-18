c599d154de48c0b5929ada86afa126641c7001de
alsheroziy <raxmatshehroz1@gmail.com>
2024-08-19 01:34:29 +0500

    navbar footer

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+// navlinks
+
+export const navLinks = [
+	{
+		id: 1,
+		path: '#',
+		name: 'Home'
+	},
+	{
+		id: 2,
+		path: '#about',
+		name: 'About'
+	},
+	{
+		id: 3,
+		path: '#services',
+		name: 'Services'
+	},
+	{
+		id: 4,
+		path: '#portfolio',
+		name: 'Portfolio'
+	},
+	{
+		id: 5,
+		path: '#contact',
+		name: 'Contact'
+	}
+]
+
+// images
